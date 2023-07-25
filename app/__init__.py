57dc81fa01a0fb96214bcec9e98e2cbcd4e0c6a5
Jiya Patel <128255337+jiyapatel4@users.noreply.github.com>
2023-07-25 09:28:28 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,11 +13,15 @@ load_dotenv()
 app = Flask(__name__)
 
 # use MySQLDatabase class to connect with a MySQL database on network
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-              user=os.getenv("MYSQL_USER"),
-              password=os.getenv("MYSQL_PASSWORD"),
-              host=os.getenv("MYSQL_HOST"),
-              port=3306         
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+  mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                user=os.getenv("MYSQL_USER"),
+                password=os.getenv("MYSQL_PASSWORD"),
+                host=os.getenv("MYSQL_HOST"),
+                port=3306         
         )
 
 print(mydb)
